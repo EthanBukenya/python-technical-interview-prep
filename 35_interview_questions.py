fcdc36e49586dea8ac70797cd50a6a9c59143bf4
EthanBukenya <ethancylb@gmail.com>
2025-12-29 13:34:42 +0300

    added two_sum() function

--- 35_interview_questions.py
+++ 35_interview_questions.py
@@ -1,6 +1,7 @@
 # 1) Two Sum Problem – Efficient Python Solution
 # Problem Statement
-# You are given an array of numbers and a target sum. Your task is to find the indices of the two numbers that add up to the target.
+# You are given an array of numbers and a target sum. Your task is to find the indices of the
+#  two numbers that add up to the target.
 
 # Example
 # nums = [2, 7, 11, 15]
@@ -8,19 +9,25 @@
 # # Output: [0, 1] (Because 2 + 7 = 9)
 
 # Optimized Approach – Hash Map
-# Instead of using a brute-force approach with two loops, we can solve this problem efficiently using a hash map (dictionary).
+# Instead of using a brute-force approach with two loops, we can solve this problem efficiently
+#  using a hash map (dictionary).
 
-# def two_sum(nums, target):
-#     num_map = {}
-#     for i, num in enumerate(nums):
-#         complementary = target - num
-#         if complementary in num_map:
-#             return [num_map[complementary], i]
-#         num_map[num] = i
-#     return []
-# # Why is this efficient?
-# # Time Complexity: O(n) – We iterate through the list once.
-# # Space Complexity: O(n) – Stores numbers and their indices in a dictionary.
+# import heapq
+# from collections import OrderedDict
+
+def two_sum(nums, target):
+    num_map = {}
+    for i, num in enumerate(nums):
+        complementary = target - num
+        if complementary in num_map:
+            return [num_map[complementary], i]
+        num_map[num] = i
+    return []
+
+
+# Why is this efficient?
+# Time Complexity: O(n) – We iterate through the list once.
+# Space Complexity: O(n) – Stores numbers and their indices in a dictionary.
 
 
 # if __name__ == '__main__':
@@ -44,9 +51,7 @@
 
 # 3. Converting a List into a String or tuple or set
 
-from collections import OrderedDict
-import heapq
-
+# length of the longest substring
 
 def length_of_longest_substring(s: str) -> int:
     char_index_map = {}
@@ -59,8 +64,8 @@ def length_of_longest_substring(s: str) -> int:
         max_length = max(max_length, i - start + 1)
     return max_length
 
-# 10. Find the Kth Largest Element in an Array
 
+# 10. Find the Kth Largest Element in an Array
 
 def find_kth_largest(nums: list, k: int) -> int:
     return heapq.nlargest(k, nums)[-1]
@@ -86,6 +91,8 @@ def has_cycle(head: ListNode) -> bool:
 
 if __name__ == '__main__':
     nums = [2, 4, 9, 0, 3, 22, 1, 20, 12]
+
+    # print(two_sum(nums, 6))
 
     days = ['S', 'M', 'T', 'W', 'Th', 'F', 'St']
     # list_to_string = ' '.join(day)
--- Advanced_interview_questions.py
+++ Advanced_interview_questions.py
@@ -2,7 +2,10 @@
 # decorators are functions that modify behaviors of other functions and are usually
 # implemented for logging, access control and caching
 
-# how to implement a decorator
+# 1 how to implement a decorator
+
+import gc
+
 
 def decorator(func):
     def wrapper(*args, **kwargs):
@@ -17,6 +20,11 @@ def decorator(func):
 def greet(name):
     print(f'hello {name}')
 
+# 2. Explain Python generators and the yield keyword.
+# Answer:
+# Generators are iterators that lazily produce values one at a time, using yield instead of return.
+# They are memory-efficient for large datasets.
+
 
 def fibonacci(n):
     a, b = 0, 1
@@ -25,8 +33,27 @@ def fibonacci(n):
         a, b = b, a+b
 
 
+# 3. How does Python manage memory?
+# Python manages memory by using reference counting and garbage collection
+a = [1, 2, 3, 4]
+b = a
+del a
+gc.collect()
+
+# 4. What are Python’s *args and **kwargs?
+# *args collects positional arguments as a tuple
+# **kwargs collects keyword arguments as a dict
+
+
+def func(*args, **kwargs):
+    print('positinal', args)
+    print('keyword', kwargs)
+
+
 if __name__ == '__main__':
    # greet('Alisha')
 
     for num in fibonacci(5):
         print(num)
+
+    func(1, 2, y=5, z=10)
